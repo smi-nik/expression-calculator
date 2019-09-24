9967537c3fcc0e2ab615fd43f6685ae8948371a7
Nikolay <n.smirnov.kz@gmail.com>
2019-09-24 09:52:49 +0600

    submit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,21 @@ function eval() {
 
 function expressionCalculator(expr) {
     // write your solution here
+    const expArray = expr.split('')
+    const checkLeftBrackets = expArray.filter(symbol => symbol === '(').length;
+    const checkRightBrackets = expArray.filter(symbol => symbol ===')').length;
+	
+    if (checkLeftBrackets !== checkRightBrackets) {
+        throw new Error ('ExpressionError: Brackets must be paired')
+    } else {
+        let result = (new Function('return ' + expr))();
+        
+		if(result === Infinity) {
+            throw new Error ('TypeError: Devision by zero.')
+        }
+		
+        return result
+     }
 }
 
 module.exports = {
